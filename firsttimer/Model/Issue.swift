02b0eb27b74f028c592fed98b161f9d45f84117e
aralatpulat <aralatpulat@gmail.com>
2019-04-07 20:41:43 +0300

    repoUrl, number, repoName and numberWithSign variables added

--- firsttimer/Model/Issue.swift
+++ firsttimer/Model/Issue.swift
@@ -11,11 +11,27 @@ import Foundation
 struct Issue: Codable {
     var id: Int
     var htmlUrl: String
+    var repoUrl: String
     var title: String
+    var number: Int
     
     enum CodingKeys: String, CodingKey {
         case id
         case htmlUrl = "html_url"
+        case repoUrl = "repository_url"
         case title
+        case number
+    }
+}
+
+extension Issue {
+    
+    var repoName: String  {
+        let splittedRepo = self.repoUrl.components(separatedBy: "/")
+        return "\(splittedRepo[4])/\(splittedRepo[5])"
+    }
+    
+    var numberWithSign: String {
+        return "#\(self.number)"
     }
 }
