d33bb1db99162a78111a317544c2d11f34216a59
aralatpulat <aralatpulat@gmail.com>
2019-04-25 20:48:48 +0300

    State logic expended & fetched array count check moved to function

--- firsttimer/Scenes/Issues/SearchViewModal.swift
+++ firsttimer/Scenes/Issues/SearchViewModal.swift
@@ -12,24 +12,52 @@ class SearchViewModal {
     
     var issues: Search = Search()
     var pageCount: Int = 1
+    var searchedText: String = ""
+    var isPagingFinished: Bool = false
     let apiClient = ApiClient()
-    var state: ((SearchState) -> ())?
-
+    var stateHandler: ((SearchState) -> ())?
+    
+    var state: SearchState! {
+        didSet {
+            self.stateHandler?(state)
+        }
+    }
+    
+    var page: String {
+        return "\(pageCount)"
+    }
+    
     func getIssues(){
-        let page = "\(pageCount)"
-        apiClient.getIssues(page: page) { [weak self] (data) in
+        self.state = SearchState.fetching
+        
+        apiClient.getIssues(page: page, search: searchedText) { [weak self] (data) in
+            
             self?.issues = data
-            self?.state?(SearchState.updated)
+            self?.state = SearchState.searched            
+            self?.isPagingFinished = self?.checkIfCountIsZero(data.items) ?? false
         }
     }
     
     func loadMoreIfNeeded(currentIndex: Int){
-        if currentIndex == issues.items.count - 1 {
+        if currentIndex == issues.items.count - 1 && !isPagingFinished {
             pageCount += 1
-            apiClient.getIssues(page: "\(pageCount)") { [weak self] (data) in
+            
+            if self.state == SearchState.fetching { return }
+            
+            self.state = SearchState.fetching
+            apiClient.getIssues(page: page, search: searchedText) { [weak self] (data) in
+
                 self?.issues.items.append(contentsOf: data.items)
-                self?.state?(SearchState.updated)
+                self?.state = SearchState.fetched
+                self?.isPagingFinished = self?.checkIfCountIsZero(data.items) ?? false
             }
         }
+    }
+}
+
+extension SearchViewModal {
+    
+    private func checkIfCountIsZero(_ items: [Issue]) -> Bool {
+        return items.count == 0
     }
 }
